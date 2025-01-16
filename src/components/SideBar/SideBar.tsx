b2d31bed02915536788396b2b5a4d85b50b8735e
Perez Elías <eg378556@gmail.com>
2025-01-16 03:14:22 -0300

    Avances en el SideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,5 +1,6 @@
 import styles from "./SideBar.module.css"
-
+import LinkCards from "../cardLinks/LinkCard"
+import { Mail, Linkedin, FileUser, MapPin } from 'lucide-react';
 
 export default function SideBar() {
 
@@ -19,7 +20,40 @@ export default function SideBar() {
                             <p>FrontEnd | TypeScript | JavaScript | React | Docker | Git | Bootstrap | Github | HTML | CSS | ...</p>
                         </div>
                     </div>
+                    <div className={styles.sectionlinedivcontainer}>
+                        <div className={styles.sectionline}></div>
+                    </div>
+
+                    <section className={styles.sectionLinks}>
+                        <LinkCards
+                            title="Email"
+                            description="eg378556@gmail.com"
+                            icon={<Mail color="#0097b2" />}
+                            link="mailto:eg378556@gmail.com"
+                        />
+                        <LinkCards
+                            title="Linkedin"
+                            description="Perez, Elías Leonel"
+                            icon={<Linkedin color="#0097b2" />}
+                            link="https://www.linkedin.com/in/eliasperez-"
+                        />
+                        <LinkCards
+                            title="CV / Resumé"
+                            description="Ver CV"
+                            icon={<FileUser color="#0097b2" />}
+                            link="https://drive.google.com/file/d/1-zA_XON9SOBT4sSgVCnWR5tqafNV2aqg/view?usp=sharing"
+                        />
+                        <LinkCards
+                            title="Ubicación"
+                            description="Argentina, Chaco"
+                            icon={<MapPin color="#0097b2" />}
+                            link=""
+                        />
+
+                    </section>
                 </section>
+
+
 
             </article>
         </>
---
+++ src/components/cardLinks/LinkCard.tsx
@@ -0,0 +1,27 @@
+import styles from './LinkCard.module.css'
+
+type CardProps = {
+    icon: any,
+    title: string,
+    description: string
+    link: string;
+}
+
+export default function LinkCards({ icon, title, description, link }: CardProps) {
+
+
+    return (
+        <>
+            <article className={styles.containerLC}>
+                <section className={styles.containerLCSection}>
+                    <div className={styles.containerIcon}>{icon}</div>
+                    <div className={styles.containerText}>
+                        <p>{title}</p>
+                        <a href={link} target="_blank">{description}</a>
+                    </div>
+                </section>
+            </article>
+        </>
+    )
+
+}

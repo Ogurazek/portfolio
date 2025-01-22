66db8142904e1eae620fc74b98f3f255d2c1545d
Perez Elías <eg378556@gmail.com>
2025-01-21 21:21:14 -0300

    Solucionando problema de las imagenes

--- src/components/Proyectos/proyectos.tsx
+++ src/components/Proyectos/proyectos.tsx
@@ -31,7 +31,7 @@ const proyectos = [
         tecnologias: 'React | TypeScript | HTML | CSS...',
         link: 'https://podcast-player-ytmusic.vercel.app',
         description: 'He desarrollado una aplicación clon de YouTube Music utilizando React y TypeScript, con el entorno de desarrollo Vite. La aplicación está conectada a una API de Podcasts para obtener los audios.',
-        img: '/public/ProyectoYTMusic.png',
+        img: '/ProyectoYTMusic.png',
         linkGit: 'https://github.com/Ogurazek/podcast-player'
     },
     {
@@ -40,7 +40,7 @@ const proyectos = [
         tecnologias: 'React | TypeScript | HTML | Docker...',
         link: 'https://podcast-player-ytmusic.vercel.app',
         description: 'Desarrollé una pasarela de pago que permite ingresar y verificar datos de tarjetas de crédito, número de tarjeta y código de seguridad. También, permite ver el precio de un objeto en Bitcoin a través de una API',
-        img: '/public/ProyectoPasarelaPago.png',
+        img: '/ProyectoPasarelaPago.png',
         linkGit: 'sd'
     },
     {
@@ -49,7 +49,7 @@ const proyectos = [
         tecnologias: 'React | TypeScript | HTML | Docker...',
         link: 'https://poke-api-xi-five.vercel.app',
         description: 'Desarrollé una Pokedex interactiva que muestra todos los Pokémon, permitiendo a los usuarios filtrar, buscar y visualizar detalles específicos de cada uno. La aplicación está conectada a la PokeApi para obtener los pokemones actualizados',
-        img: '/public/ProyectoPokedex.png',
+        img: '/ProyectoPokedex.png',
         linkGit: 'https://github.com/Ogurazek/PokeApi'
     },
     {
@@ -58,7 +58,7 @@ const proyectos = [
         tecnologias: 'React | TypeScript | HTML | CSS...',
         link: 'https://instagram-clon-phi.vercel.app',
         description: 'Desarrollé un clon de la página de Instagram utilizando TypeScript y React. La aplicación replica varias funcionalidades clave de la plataforma original, brindando una experiencia interactiva y moderna para los usuarios.',
-        img: '/public/ProyectoIG.png',
+        img: '/ProyectoIG.png',
         linkGit: 'https://github.com/Ogurazek/Instagram-clon'
     },
     {
@@ -67,7 +67,7 @@ const proyectos = [
         tecnologias: 'HTML | JavaScript | Bootstrap...',
         link: 'https://www.youtube.com/watch?v=b-MvlaW83h8',
         description: 'Participé en el desarrollo de un blog en equipo utilizando Scrum para una gestión eficiente del tiempo y los recursos. Implementamos interfaces atractivas empleando JavaScript, HTML, CSS y Bootstrap.',
-        img: '/public/ProyectoWeb.png',
+        img: '/ProyectoWeb.png',
         linkGit: '...'
     },
 ]
